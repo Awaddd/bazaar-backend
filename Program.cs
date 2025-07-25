6003a25b2aa515dbea9862cda8d41a3ded0f81b0
Awaddd <34748292+Awaddd@users.noreply.github.com>
2025-07-25 22:25:06 +0100

    Fix cors and update temp

--- Program.cs
+++ Program.cs
@@ -3,6 +3,16 @@ using Commerce.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowNextJs", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite("Data Source=commerce.db"));
@@ -20,5 +30,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.UseStaticFiles();
+app.UseCors("AllowNextJs");
 app.MapControllers();
 app.Run();
